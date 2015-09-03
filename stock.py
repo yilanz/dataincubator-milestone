a46320d2ddf46f76b8977b08995b9d90d093d8d2
yilan <yilan@umich.edu>
2015-09-03 17:18:15 -0400

    setting up the app

--- stock.py
+++ stock.py
@@ -3,7 +3,7 @@ app_stock = Flask(__name__)
 
 @app_stock.route('/')
 def stock_index():
-	return "Hello World!"
+	return render_template('index.html')
 	
 if __name__ == '__main__':
 	app_stock.run(debug = True)

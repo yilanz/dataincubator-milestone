f09a59067405842c414c3ffb652d5f538ed9b333
yilan <yilan@umich.edu>
2015-09-03 19:03:34 -0400

    setting up the app

--- app.py
+++ app.py
@@ -14,9 +14,12 @@ def stock_func():
 	else:
 		app.vars['stock_name'] = request.form['stock_name']
 		app.vars['features'] = request.form['features']
-		return render_template('stock_plot.html',stock_name = app.vars['stock_name'],features=app.vars['features'])
-#@app.route('/stock_plot',methods=['POST'])
-#def stock_plot():
+		return redirect(url_for('stock_plot',app.vars))
+
+@app.route('/stock_plot',methods=['POST'])
+def stock_plot(app.vars):
+	render_template('stock_plot.html',stock_name = app.vars['stock_name'],features=app.vars['features'])
+
 	
 if __name__ == '__main__':
   app.run(port=33507,debug = True)

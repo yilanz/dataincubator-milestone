85a38849ffcf7d9d449d503a4593d95fb397f220
yilan <yilan@umich.edu>
2015-09-03 18:05:16 -0400

    setting up the app

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def main():
   return redirect('/index')
 
-@app.route('/index',methods=['GET','POST'])
+@app.route('/index', methods=['GET','POST'])
 def stock_index():
-nquestion = 4
-if request.method == 'GET':
-	return render_template('stock_index.html')
-else:
-	return 'reqeust.method was not a GET!'
+	nquestion = 4
+	if request.method == 'GET':
+		return render_template('stock_index.html')
+	else:
+		return 'reqeust.method was not a GET!'
 
 if __name__ == '__main__':
   app.run(port=33507,debug = True)
